8aa813ddc69aa7614658b4ae6084d00970010583
Sruthi-Latchupatula987 <116361314+Sruthi-Latchupatula987@users.noreply.github.com>
2022-11-09 14:50:30 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+              static int a=100;
+              static double d=12.75;
+              static char ch='z';
+              static String str="Welcome";
+              public static void main(String args[]) {
+            	  System.out.println(a);
+            	  System.out.println(d);
+            	  System.out.println(ch);
+            	  System.out.println(str);
+            	  
+              }
+}
